d51b900bcb8d8810f48a4c2d49b240b746ba4b4b
Taiyo Akashi <onibakidd@gmail.com>
2017-01-07 17:42:26 +0900

    fix to oop

--- lesson160107/ticket_checker.rb
+++ lesson160107/ticket_checker.rb
@@ -1,71 +1,60 @@
 STATIONS_LIST = ["梅田", "十三", "庄内", "岡町"]
 
 class Ticket
-  attr_accessor :price
-  attr_reader :from_station, :to_station
+  attr_accessor :price, :from_station, :to_station
 
-  def initialize
+  def initialize price
+    @price = price
     @from_station = nil
     @to_station = nil
   end
-
-  def set_from_station station_name
-    if @from_station.nil?
-      @from_station = station_name
-      true
-    else
-      puts "再入場出来ません"
-      false
-    end
-  end
-
-  def set_to_station station_name
-    if @to_station.nil?
-      @to_station = station_name
-      true
-    else
-      puts "再出場出来ません"
-      false
-    end
-  end
 end
 
-class TicketChecker
-  attr_accessor :ticket
+class TicketGate
+  attr_accessor :message
 
-  # def initialize station
-  #   @station = station
-  # end
-  #
-  # def in_to_station ticket
-  #   @ticket  
-  # end
-  #
-  # def out_to_station station
-  # end
+  def initialize station
+    @station = station
+    @message = nil
+  end
 
-  def required_price
-    distance = STATIONS_LIST.index(@ticket.to_station) - STATIONS_LIST.index(@ticket.from_station)
+  def go_in ticket
+    if ticket.from_station.nil?
+      ticket.from_station = @station
+    else
+      @message = "再入場出来ません"
+    end
+    ticket
+  end
+
+  def go_out ticket
+    if ticket.from_station.nil?
+       @message = "入場時に改札を通っていません。"
+    else
+      if ticket.to_station.nil?
+        if self.required_price(ticket.from_station).nil?
+          @message = "乗車駅と同じ駅です。"
+        else
+          if ticket.price < self.required_price(ticket.from_station)
+            @message = "乗車賃が足りません"
+          else
+            ticket.to_station = @station
+          end
+        end
+      else
+        @message = "再出場出来ません"
+      end
+    end
+    ticket
+  end
+
+  def required_price from_station
+    distance = STATIONS_LIST.index(@station) - STATIONS_LIST.index(from_station)
     case distance.abs
     when 1 then 150
     when 2 then 180
     when 3 then 220
     when 0 then nil
-    end
-  end
-
-  def go_out
-    if @ticket.from_station.nil?
-      puts "入場時に改札を通っていません。"
-      false
-    else
-      diff_price = @ticket.price - self.required_price
-      if self.required_price.nil?
-        puts "乗車駅と同じ駅です。"
-        false
-      else
-        diff_price >= 0 ? true : false
-      end
     end
   end
 end
--- lesson160107/ticket_checker_spec.rb
+++ lesson160107/ticket_checker_spec.rb
@@ -1,196 +1,172 @@
 require './ticket_checker'
 
 # INPUT: 金額：150円 入場：梅田　出場：十三 結果：出場できる
-describe TicketChecker do
+describe TicketGate do
   describe "#1 150円の切符を購入し梅田で入場、十三で出場した結果" do
     it "出場できる" do
-      ticket = Ticket.new
-      ticket.set_from_station "梅田"
-      ticket.set_to_station "十三"
-      ticket.price = 150
+      ticket = Ticket.new(150)
+      go_in_ticket_checker = TicketGate.new("梅田")
+      ticket = go_in_ticket_checker.go_in(ticket)
+      go_out_ticket_checker = TicketGate.new("十三")
+      ticket = go_out_ticket_checker.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq true
+      expect(go_out_ticket_checker.message).to be_nil
     end
   end
 
   describe "#2 150円の切符を購入し梅田で入場、庄内で出場した結果" do
     it "出場できない" do
-      ticket = Ticket.new
-      ticket.set_from_station "梅田"
-      ticket.set_to_station "庄内"
-      ticket.price = 150
+      ticket = Ticket.new(150)
+      umeda = TicketGate.new("梅田")
+      ticket = umeda.go_in(ticket)
+      shounai = TicketGate.new("庄内")
+      ticket = shounai.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq false
-
+      expect(shounai.message).not_to be_nil
     end
   end
 
   describe "#3 180円の切符を購入し梅田で入場、庄内で出場した結果" do
     it "出場できる" do
-      ticket = Ticket.new
-      ticket.set_from_station "梅田"
-      ticket.set_to_station "庄内"
-      ticket.price = 180
+      ticket = Ticket.new(180)
+      go_in_ticket_checker = TicketGate.new("梅田")
+      ticket = go_in_ticket_checker.go_in(ticket)
+      go_out_ticket_checker = TicketGate.new("庄内")
+      ticket = go_out_ticket_checker.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq true
-
+      expect(go_out_ticket_checker.message).to be_nil
     end
   end
 
   describe "#4 220円の切符を購入し梅田で入場、庄内で出場した結果" do
     it "出場できる" do
-      ticket = Ticket.new
-      ticket.set_from_station "梅田"
-      ticket.set_to_station "庄内"
-      ticket.price = 220
+      ticket = Ticket.new(220)
+      go_in_ticket_checker = TicketGate.new("梅田")
+      ticket = go_in_ticket_checker.go_in(ticket)
+      go_out_ticket_checker = TicketGate.new("庄内")
+      ticket = go_out_ticket_checker.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq true
-
+      expect(go_out_ticket_checker.message).to be_nil
     end
   end
 
   describe "#5 180円の切符を購入し梅田で入場、岡町で出場した結果" do
     it "出場できない" do
-      ticket = Ticket.new
-      ticket.set_from_station "梅田"
-      ticket.set_to_station "岡町"
-      ticket.price = 180
+      ticket = Ticket.new(180)
+      go_in_ticket_checker = TicketGate.new("梅田")
+      ticket = go_in_ticket_checker.go_in(ticket)
+      go_out_ticket_checker = TicketGate.new("岡町")
+      ticket = go_out_ticket_checker.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq false
-
+      expect(go_out_ticket_checker.message).not_to be_nil
     end
   end
 
   describe "#6 220円の切符を購入し梅田で入場、岡町で出場した結果" do
     it "出場できる" do
-      ticket = Ticket.new
-      ticket.set_from_station "梅田"
-      ticket.set_to_station "岡町"
-      ticket.price = 220
+      ticket = Ticket.new(220)
+      go_in_ticket_checker = TicketGate.new("梅田")
+      ticket = go_in_ticket_checker.go_in(ticket)
+      go_out_ticket_checker = TicketGate.new("岡町")
+      ticket = go_out_ticket_checker.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq true
+      expect(go_out_ticket_checker.message).to be_nil
     end
   end
 
   describe "#7 150円の切符を購入し十三で入場、岡町で出場した結果" do
     it "出場できない" do
-      ticket = Ticket.new
-      ticket.set_from_station "十三"
-      ticket.set_to_station "岡町"
-      ticket.price = 150
+      ticket = Ticket.new(150)
+      go_in_ticket_checker = TicketGate.new("十三")
+      ticket = go_in_ticket_checker.go_in(ticket)
+      go_out_ticket_checker = TicketGate.new("岡町")
+      ticket = go_out_ticket_checker.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq false
+      expect(go_out_ticket_checker.message).not_to be_nil
     end
   end
 
   describe "#8 180円の切符を購入し十三で入場、岡町で出場した結果" do
     it "出場できる" do
-      ticket = Ticket.new
-      ticket.set_from_station "十三"
-      ticket.set_to_station "岡町"
-      ticket.price = 180
+      ticket = Ticket.new(180)
+      go_in_ticket_checker = TicketGate.new("十三")
+      ticket = go_in_ticket_checker.go_in(ticket)
+      go_out_ticket_checker = TicketGate.new("岡町")
+      ticket = go_out_ticket_checker.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq true
+      expect(go_out_ticket_checker.message).to be_nil
     end
   end
 
   describe "#9-#8 180円の切符を購入し岡町で入場、十三で出場した結果" do
     it "出場できる" do
-      ticket = Ticket.new
-      ticket.set_from_station "岡町"
-      ticket.set_to_station "十三"
-      ticket.price = 180
+      ticket = Ticket.new(180)
+      go_in_ticket_checker = TicketGate.new("岡町")
+      ticket = go_in_ticket_checker.go_in(ticket)
+      go_out_ticket_checker = TicketGate.new("十三")
+      ticket = go_out_ticket_checker.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq true
+      expect(go_out_ticket_checker.message).to be_nil
     end
   end
 
   describe "#9-#7 150円の切符を購入し岡町で入場、十三で出場した結果" do
     it "出場できない" do
-      ticket = Ticket.new
-      ticket.set_from_station "岡町"
-      ticket.set_to_station "十三"
-      ticket.price = 150
+      ticket = Ticket.new(150)
+      go_in_ticket_checker = TicketGate.new("岡町")
+      ticket = go_in_ticket_checker.go_in(ticket)
+      go_out_ticket_checker = TicketGate.new("十三")
+      ticket = go_out_ticket_checker.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq false
+      expect(go_out_ticket_checker.message).not_to be_nil
     end
   end
 
-  describe "#10 150円の切符を購入し岡町で入場、十三で出場した結果" do
+  describe "#10 150円の切符を購入し梅田で入場、梅田で出場した結果" do
     it "出場できない" do
-      ticket = Ticket.new
-      ticket.set_from_station "岡町"
-      ticket.set_to_station "十三"
-      ticket.price = 150
+      ticket = Ticket.new(150)
+      umeda_in = TicketGate.new("梅田")
+      ticket = umeda_in.go_in(ticket)
+      umeda_out = TicketGate.new("梅田")
+      ticket = umeda_out.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq false
+      expect(umeda_out.message).not_to be_nil
     end
   end
 
   describe "#11 150円の切符を購入し梅田で入場、さらに梅田で再入場した結果" do
     it "入場できない" do
-      ticket = Ticket.new
-      ticket.set_from_station "梅田"
-      ticket.price = 150
+      ticket = Ticket.new(150)
+      go_in_ticket_checker = TicketGate.new("梅田")
+      ticket = go_in_ticket_checker.go_in(ticket)
+      go_in_ticket_checker2 = TicketGate.new("梅田")
+      ticket = go_in_ticket_checker2.go_in(ticket)
 
-      expect(ticket.set_from_station "梅田").to eq false
+      expect(go_in_ticket_checker2.message).not_to be_nil
     end
   end
 
   describe "#12 150円の切符を購入し梅田で入場、十三で出場し、再度十三で出場した結果" do
     it "出場できない" do
-      ticket = Ticket.new
-      ticket.set_from_station "梅田"
-      ticket.set_to_station "十三"
-      ticket.price = 150
+      ticket = Ticket.new(150)
+      go_in_ticket_checker = TicketGate.new("梅田")
+      ticket = go_in_ticket_checker.go_in(ticket)
+      go_out_ticket_checker = TicketGate.new("十三")
+      ticket = go_out_ticket_checker.go_out(ticket)
+      go_out_ticket_checker2 = TicketGate.new("十三")
+      ticket = go_out_ticket_checker2.go_out(ticket)
 
-      expect(ticket.set_to_station "十三").to eq false
+      expect(go_out_ticket_checker2.message).not_to be_nil
     end
   end
 
   describe "#13 150円の切符を購入し改札を通さず入場して、梅田で出場した結果" do
     it "出場できない" do
-      ticket = Ticket.new
-      ticket.set_to_station "梅田"
-      ticket_price = 150
+      ticket = Ticket.new(150)
+      go_out_ticket_checker = TicketGate.new("十三")
+      ticket = go_out_ticket_checker.go_out(ticket)
 
-      ticket_checker = TicketChecker.new
-      ticket_checker.ticket = ticket
-
-      expect(ticket_checker.go_out).to eq false
+      expect(go_out_ticket_checker.message).not_to be_nil
     end
   end
 end
