aede9b0ac37599c7c47fe2f9e7256b53102b0b32
Taiyo Akashi <onibakidd@gmail.com>
2017-11-11 12:50:11 +0900

    refactoring to class

--- 171111/warmup.rb
+++ 171111/warmup.rb
@@ -1,60 +1,61 @@
+class Matrix
+  def initialize(string)
+    @_matrix = string2matrix(string)
+  end
+
+  #
+  # 転置する
+  #
+  def transpose
+    transposed_matrix = []
+
+    @_matrix[0].size.times.each do |_|
+      transposed_matrix << []
+    end
+
+    @_matrix.each.with_index(0) do |array, line|
+      array.each.with_index(0) do |elm, row|
+        transposed_matrix[row] << elm
+      end
+    end
+
+    @_matrix = transposed_matrix
+  end
+
+  def output
+    @_matrix.each do |line_array|
+      puts line_array.join(' ')
+    end
+  end
+
+  private
+  #
+  # @param [String]
+  #
+  def string2matrix(string)
+    matrix = []
+    string.each_line do |line|
+      array = line.split(' ')
+      matrix << array
+    end
+    matrix
+  end
+end
+
 input = <<~EOS
   1 2 3
   4 5 6
   7 8 9
 EOS
 
+
+### EXECUTION #######
+
 puts "---before-----"
-
 puts input
-
-#
-# @param [String]
-#
-def string2matrix(string)
-  matrix = []
-  string.each_line do |line|
-    array = line.split(' ')
-    matrix << array
-  end
-  matrix
-end
-
-def transpose(matrix)
-  transposed_matrix = []
-
-  matrix[0].size.times.each do |_|
-    transposed_matrix << []
-  end
-
-  matrix.each.with_index(0) do |array, line|
-    array.each.with_index(0) do |elm, row|
-      transposed_matrix[row] << elm
-    end
-  end
-
-  transposed_matrix
-end
-
-#
-# @param [Array] e.g. [[1,2,3], [2,3,4], [4,5,6]]
-#
-def output(matrix)
-  matrix.each do |line_array|
-    puts line_array.join(' ')
-  end
-end
-
-matrix = string2matrix(input)
 
 puts "---after-----"
 
-output(transpose(matrix))
-
-# class Matrix
-#   def initialize(string)
-#   end
-#
-#   def output
-#   end
-# end
+new_matrix = Matrix.new(input)
+new_matrix.transpose
+new_matrix.output
